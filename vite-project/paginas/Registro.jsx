02d29e73d1f06d468ed20765db18b7e092a8085d
Raquel <raquellopezsancheez@gmail.com>
2025-04-16 21:44:13 +0200

    pagina inicio sesion y resgistrarse listas

---
+++ vite-project/paginas/Registro.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Registro = ()=>{
+
+    const [Valor, setValor] = useState({nombreusuario:"", email:"", contraseña:""})
+
+    return (
+        
+        <div className="flex h-screen flex-col items-center justify-center">
+            {/*investigando descubrí que la medida vw equivale al % en css, por lo tanto tiene un with de 60% */}
+            <div className="w-[60vw] md:w-[50vw] lg:w-[30vw]">
+                <h1 className="text-3xl font-bold text-center mb-1 text-[#815BCE]">
+                    Flowlist
+                </h1>
+                <h3 className="text-center font-semibold text-[#A4A2A2] ">Registrate en Flowlist</h3>
+                <div className="w-[60vw] md:w-[50vw] lg:w-[30vw] mt-4">
+                    <form className="flex flex-col gap-4">
+                        <input type="text" required placeholder="nombre de usuario" className="border rounded px-4 py-2 border-[#FA9F42] w-[100%] outline-none placeholder-[#A4A2A2] text-[#FFB468] focus:shadow-[0_0_10px_#FA9F42]" name="nombreusuario" value={Valor.nombreusuario}/>
+
+                        <input type="email" required placeholder="email" className="border rounded px-4 py-2 border-[#FA9F42] w-[100%] outline-none placeholder-[#A4A2A2] text-[#FFB468] focus:shadow-[0_0_10px_#FA9F42]" name="email" value={Valor.email}/>
+
+                        <input type="contraseña" required placeholder="contraseña" className="border rounded px-4 py-2 border-[#FA9F42] w-[100%] outline-none placeholder-[#A4A2A2] text-[#FFB468] focus:shadow-[0_0_10px_#FA9F42]" name="contraseña" value={Valor.contraseña} />
+
+                        <button className="bg-[#FA9F42] text-white font-semibold py-2 rounded hover:bg-[#FFB972]   transition all duration-300">Registrarse</button>
+
+                        <p className="text-center text-[#A4A2A2]">
+                            ¿Ya tienes una cuenta? <Link to={"/Login"} className="text-[#815BCE] font-semibold">Iniciar sesión</Link>
+                        </p>
+
+
+                       
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default Registro
